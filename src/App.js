fdb76bdc90715c50503a3c62eff93aca5ce2b593
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-12-18 21:52:41 -0800

    setting state with hooks

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+import { render } from 'react-dom'
+import SearchParams from './SearchParams';
+
+
+const App = () => {
+
+    return (
+        <div>
+            <h1 id="something-important">Adopte Me!</h1>
+            <SearchParams />
+        </div>
+    )
+};
+
+render(<App />,
+    document.getElementById("root"));
+
