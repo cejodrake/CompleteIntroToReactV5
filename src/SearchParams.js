f4095c3d471dc790e3d089d7098072ad38dd3aab
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-12-19 10:32:06 -0800

    Calling the Api pet and use Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
+import { ANIMALS } from "@frontendmasters/pet"
+
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seatle, Wa")
+    const [location, setLocation] = useState("Seatle,Wa");
+    const [animal, setAnimal] = useState("dog");
 
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
-                    Location
+                    <h1>{location}</h1>
                     <input id="location"
                         value={location}
                         placeholder="Location"
                         onChange={e => setLocation(e.target.value)} />
 
+                </label>
+
+                <label htmlFor="animal">
+                    ANIMALS
+                    <select id="animal" value={animal} onChange={e => setAnimal(e.target.value)} onBlur={e => setAnimal(e.target.value)}>
+                        <option>All</option>
+                        {ANIMALS.map(animal => (<option key={animal} value={animal}> {animal}</option>))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
